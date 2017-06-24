6cd559a9ec46745c0880566aa143aa8f13a21a2f
Ivanov Alexey <aleksusola@yandex.ru>
2017-06-24 12:49:14 +0300

    checkpoint1

---
+++ app/src/main/java/com/aleksus/handtohand1/SearchActivity.java
@@ -0,0 +1,20 @@
+package com.aleksus.handtohand1;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
